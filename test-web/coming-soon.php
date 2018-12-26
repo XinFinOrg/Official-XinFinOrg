930e91e2f9749949bc03f8656978270e64e53b84
Dattatray <41412142+Dattatrayh@users.noreply.github.com>
2018-12-26 20:43:18 +0530

    Home page changes
    Menubar changes,
    What is XinFin section added on home page,
    Partners section changes,
    Media section changes,
    style.css and bootstrap.min.css changes

---
+++ test-web/coming-soon.php
@@ -0,0 +1,26 @@
+<?php 
+$title = "Coming Soon";
+$desc = "";
+
+include('inc/header.php') ?>
+<section id="banner"  style=" background: url('assets/images/xdc-protocol-banner-image2.png') #112b3c;
+     background-repeat: no-repeat;
+    background-position: bottom right; min-height:700px"> 
+ <!--  <div id="particle-canvas" ></div> -->
+  <div class="container"><div class="row"><div class="bannertext1"><div class="col-md-12" style="padding-top:5%"><h1     style=" padding-top: 10%;  ">COMING SOON</h1>
+  <p>
+
+</p>
+</div>
+
+
+
+
+</div></div></div> 
+
+  </section>
+
+
+
+
+<?php include('inc/footer.php') ?>
---
+++ test-web/inc/custom_function.php
@@ -0,0 +1,7 @@
+<?php  
+
+	function base_url(){
+
+		$base_url = 'http://localhost/xinfin_new/';
+		return $base_url;
+	}
