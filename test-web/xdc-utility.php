265ab99a50425372b44b15cf4b3bfc388dab5594
Dattatray <41412142+Dattatrayh@users.noreply.github.com>
2018-12-28 21:41:52 +0530

    xdc-utility page changes
    style.css changes,
    bootstrap.min.css changes

--- test-web/xdc-utility.php
+++ test-web/xdc-utility.php
@@ -18,31 +18,94 @@ include('inc/header.php') ?>
 
 <section id="xdc-utility">
   <div class="container">
-  	<div class="row">
-    <div class="col-md-4 col-sm-6 col-xs-12">
+  	<div class="row flex-row">
+    <div class="col-md-4 col-sm-6 col-xs-12 mb-5">
         <div class="usecase-box">
             <div class="image-container">
             	<img class="img-responsive" src="assets/images/utility/mycontract-logo.jpg" alt="mycontract.co">
             </div>
             <h3>MyContract</h3>
-            <div class="githublink"><a href="https://github.com"><i class="fa fa-github"></i> https://github.com</a></div>
+            <div class="githublink"><a href="https://github.com/XinFinOrg/ico-automation-dashboard"><i class="fa fa-github"></i> https://github.com/XinFinOrg/ico-automation-dashboard</a></div>
             <div class="githublink"><a href="https://www.mycontract.co"><i class="fa fa-external-link"></i> https://www.mycontract.co</a></div>
-            <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>MyContract.co is a WebApp that allows users to create smart contracts for issuing tokens to crowdsale or for asset tokenization without a need for any programming expertise, just in a few clicks.</p>
             <div class="githublink"><a href="API Link">API Link will come here</a></div>
-            <div class="githublink"><a href="https://www.youtube.com/channel/UCQaL6FixEQ80RJC0B2egX6g"><i class="fa fa-youtube"></i> Watch Video</a></div>
-            <p class="date">Utility for XDC/XDCE brief with Benefit, Industry</p>
+            <div class="githublink"><a href="https://www.youtube.com/watch?v=thR-pTpF7Sw&t=3s"><i class="fa fa-youtube"></i> Watch Video</a></div>
+            <p class="date">MyContract.co allows you to create your own token in a few minutes by just providing token basic details, such as token name, token symbol, token decimals, total supply, and token sale limit, in an easy to grasp environment. After providing all the details of the token created, and buying the package—payable in XDCE tokens — the users can easily get the code of their brand new token. MyContract will allow businesses of all sizes to deploy their own smart-contracts and tokens across industries, with working use cases in Supply Chain and Decentralized Credit applications.</p>
             <!--<div class="title">Nov 21st 2018</div>-->
         </div>
     </div>
     
-    <div class="col-md-4 col-sm-6 col-xs-12">
+    <div class="col-md-4 col-sm-6 col-xs-12 mb-5">
         <div class="usecase-box">
             <div class="image-container">
             	<img class="img-responsive" src="assets/images/utility/tradefinex-logo.jpg" alt="tradefinex.org">
             </div>
             <h3>TradeFinex</h3>
-            <div class="githublink"><a href="https://github.com"><i class="fa fa-github"></i> https://github.com</a></div>
+            <div class="githublink"><a href="https://github.com/XinFinOrg/tradefinexlive"><i class="fa fa-github"></i> https://github.com/XinFinOrg/tradefinexlive</a></div>
             <div class="githublink"><a href="https://www.mycontract.co"><i class="fa fa-external-link"></i> https://www.tradefinex.org</a></div>
+            <p>TradeFinex.org is a p2p trade finance platform to create trade instruments of value between buyers and sellers in Trade Finance market. It is an open protocol for banks, institutions and users and consortium-led governance makes it truly decentralized.</p>
+            <div class="githublink"><a href="https://xinfinorg.github.io/TradeFinex_API/">https://xinfinorg.github.io/TradeFinex_API/</a></div>
+            <div class="githublink"><a href="https://www.youtube.com/watch?v=mjrhTnZ16f8&t=9s"><i class="fa fa-youtube"></i> Watch Video</a></div>
+            <p class="date">TradeFinex platform will help buyers secure capital at globally competitive rates, give suppliers visibility on global tenders & customer base and finally it gives financiers real time visibility on their investments using digitization and IoT integration of their assets. It is an open API platform which is compatible with legacy systems and ISO20022 and allows to share documents, supports Decentralized Credit Data and Decentralized KYC Data with zero knowledge proofs. TradeFinex comes with other attributes including Asset/Projects Monitoring, Private Network Interactions, Smart-Contracts, Post-Trade Settlements, Counter-Party Risk Mitigation and Increased Liquidity.</p>
+        </div>
+    </div>
+    
+    <div class="col-md-4 col-sm-6 col-xs-12 mb-5">
+        <div class="usecase-box">
+            <div class="image-container">
+            	<img class="img-responsive" src="assets/images/utility/kramaa-logo.jpg" alt="kramaa.com">
+            </div>
+            <h3>Kramaa</h3>
+            <div class="githublink"><a href="https://github.com/XinFinOrg/Kramaa"><i class="fa fa-github"></i> https://github.com/XinFinOrg/Kramaa</a></div>
+            <div class="githublink"><a href="http://www.kramaa.com"><i class="fa fa-external-link"></i> http://www.kramaa.com</a></div>
+            <p>Kramaa is a Singapore based technology company converging latest technologies in blockchain, IoT, and analytics. The Company has created a blockchain based application ("The platform") for IoT identities linked to physical assets on Xinfin's proven hybrid blockchain architecture.</p>
+            <div class="githublink"><a href="API Link">API Link will come here</a></div>
+            <div class="githublink"><a href="https://www.youtube.com/channel/UCQaL6FixEQ80RJC0B2egX6g"><i class="fa fa-youtube"></i> Watch Video</a></div>
+            <p class="date">The platform securely integrates physical assets & IoT devices through blockchain, smart contracts & firmware for creating digital identity, provenance, authentication, e-commerce, supply chain & finance. Industry applications for the platform include smart cities, pharma, packaging, logistics, Fashion / Luxury, Food & beverages, precious metals & Jewelry.</p>
+            <!--<div class="title">Nov 21st 2018</div>-->
+        </div>
+    </div>
+    
+    <div class="col-md-4 col-sm-6 col-xs-12 mb-5">
+        <div class="usecase-box">
+            <div class="image-container">
+            	<img class="img-responsive" src="assets/images/utility/land-registry-logo.jpg" alt="tradefinex.org">
+            </div>
+            <h3>Land Registry</h3>
+            <div class="githublink"><a href="https://github.com/XinFinOrg/land-registry-mhgov"><i class="fa fa-github"></i> https://github.com/XinFinOrg/land-registry-mhgov</a></div>
+            <div class="githublink"><a href="http://land-registry.xinfin.org/"><i class="fa fa-external-link"></i> http://land-registry.xinfin.org/</a></div>
+            <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div class="githublink"><a href="http://land-registry.xinfin.org/docs/">http://land-registry.xinfin.org/docs/</a></div>
+            <div class="githublink"><a href="https://www.youtube.com/channel/UCQaL6FixEQ80RJC0B2egX6g"><i class="fa fa-youtube"></i> Watch Video</a></div>
+            <p class="date">Utility for XDC/XDCE brief with Benefit, Industry</p>
+            <!--<div class="title">Nov 21st 2018</div>-->
+        </div>
+    </div>
+    
+    <div class="col-md-4 col-sm-6 col-xs-12 mb-5">
+        <div class="usecase-box">
+            <div class="image-container">
+            	<img class="img-responsive" src="assets/images/utility/iFactor-logo.jpg" alt="tradefinex.org">
+            </div>
+            <h3>iFactor</h3>
+            <div class="githublink"><a href="https://github.com/XinFinOrg/ifactor-poc"><i class="fa fa-github"></i> https://github.com/XinFinOrg/ifactor-poc</a></div>
+            <div class="githublink"><a href="http://ifactor.xinfin.org/"><i class="fa fa-external-link"></i> http://ifactor.xinfin.org</a></div>
+            <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div class="githublink"><a href="ifactor.xinfin.org/docs">ifactor.xinfin.org/docs</a></div>
+            <div class="githublink"><a href="https://www.youtube.com/channel/UCQaL6FixEQ80RJC0B2egX6g"><i class="fa fa-youtube"></i> Watch Video</a></div>
+            <p class="date">Utility for XDC/XDCE brief with Benefit, Industry</p>
+            <!--<div class="title">Nov 21st 2018</div>-->
+        </div>
+    </div>
+    
+    <div class="col-md-4 col-sm-6 col-xs-12 mb-5">
+        <div class="usecase-box">
+            <div class="image-container">
+            	<img class="img-responsive" src="assets/images/utility/blockchain-education-logo.jpg" alt="tradefinex.org">
+            </div>
+            <h3>Blockchain Education</h3>
+            <div class="githublink"><a href="https://github.com/XinFinOrg/Blockchain_Tutorial-website"><i class="fa fa-github"></i> https://github.com/XinFinOrg/Blockchain_Tutorial-website</a></div>
+            <div class="githublink"><a href="https://xinfinorg.github.io/Blockchain_Tutorial-website/index.html"><i class="fa fa-external-link"></i> https://xinfinorg.github.io/Blockchain_Tutorial-website/index.html</a></div>
             <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <div class="githublink"><a href="API Link">API Link will come here</a></div>
             <div class="githublink"><a href="https://www.youtube.com/channel/UCQaL6FixEQ80RJC0B2egX6g"><i class="fa fa-youtube"></i> Watch Video</a></div>
@@ -51,14 +114,13 @@ include('inc/header.php') ?>
         </div>
     </div>
     
-    <div class="col-md-4 col-sm-6 col-xs-12">
+    <div class="col-md-4 col-sm-6 col-xs-12 mb-5">
         <div class="usecase-box">
             <div class="image-container">
-            	<img class="img-responsive" src="assets/images/utility/kramaa-logo.jpg" alt="tradefinex.org">
+            	<img class="img-responsive" src="assets/images/utility/turantPay-logo.jpg" alt="tradefinex.org">
             </div>
-            <h3>Kramaa</h3>
-            <div class="githublink"><a href="https://github.com"><i class="fa fa-github"></i> https://github.com</a></div>
-            <div class="githublink"><a href="http://www.kramaa.com"><i class="fa fa-external-link"></i> http://www.kramaa.com</a></div>
+            <h3>TurantPay</h3>
+            <div class="githublink"><a href="https://github.com/XinFinOrg/TurantPay"><i class="fa fa-github"></i> https://github.com/XinFinOrg/TurantPay</a></div>
             <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <div class="githublink"><a href="API Link">API Link will come here</a></div>
             <div class="githublink"><a href="https://www.youtube.com/channel/UCQaL6FixEQ80RJC0B2egX6g"><i class="fa fa-youtube"></i> Watch Video</a></div>
