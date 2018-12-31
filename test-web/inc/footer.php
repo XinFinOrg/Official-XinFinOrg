551ad061bc93668fecea0abbfeab135c2b73ab45
Ritesh kakkad <44856884+riteshkakkad@users.noreply.github.com>
2018-12-31 19:39:20 +0530

    Update footer.php
    change in placing

--- test-web/inc/footer.php
+++ test-web/inc/footer.php
@@ -10,7 +10,6 @@
 		<a href="xdc-utility.php">XDC/XDCe Utility</a><br/>
 		<a href="https://status.xinfin.org/">See XinFin Network Data & Status</a><br/>
 		<a href="xdc-protocol.php#masternodes">Stake XDC and Setup MasterNode</a><br/>
-		<a href="contactus.php">Press Inquiry</a><br/>	
                 <a href="https://github.com/XinFinOrg/Open-Hackathon" target="_blank">Online Hackathon</a></p>
 			</div>
 			
@@ -20,6 +19,7 @@
                 <a href="developers.php">Developers Portal</a><br/>
 		<a href="https://github.com/XinFinorg">Browse our Source Code on GitHub</a><br/>
 		<a href="POS.php">Why Proof of Stake Consensus?</a><br/>
+		<a href="contactus.php">Press Inquiry</a><br/>	
             	<a href="contactus.php">Contact XinFin</a></p>
 			</div>
 			
