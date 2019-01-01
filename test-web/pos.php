6fda345d9e3e782f2d5568c9e7f20606c2e96d2b
Ritesh kakkad <44856884+riteshkakkad@users.noreply.github.com>
2019-01-01 17:42:59 +0530

    Update pos.php
    added changes.

--- test-web/pos.php
+++ test-web/pos.php
@@ -2,7 +2,34 @@
 $title = "Why Proof of Stake ?";
 $desc = "Proof of Stake is a novel consensus to Prevent Energy";
 include('inc/header.php') ?>
+<section id="bannerdev" > 
+ <!-- particles.js container --> 
+<div id="particles-js"></div>
+
+  <div class="container"><div class="row"><div class="bannertext">
+ 
 
 
+
+  <div class="tabbable tabs-bottom">
+                <div class="tab-content">
+
+                  <div class="tab-pane active" id="developer">                
+            <div class="jumbotron">
+     
+  <div  style="display:block; position: relative" >
+<h1>Developers community</h1>
+              <p>We’ve set up this resource center to allow Xinfin community developers, enthusiasts, and other partners to discuss, code, <br> access documentation, report bugs, get advice, and support to make the XDC ecosystem the blockchain for enterprise.</p>    
+              </div>           
+
+            </div>
+          </div> 
+               
+    				<div class="tab-pane" id="learn">                
+						<div class="jumbotron">
+             <div  style="display:block; position: relative" class="devstyle">
+<!-- <h2>Developers community</h2>
+<p style="font-size: 16px !important;">Why Proof of Stake consensus </p>    --> 
+</div>
 
 <?php include('inc/footer.php') ?>
