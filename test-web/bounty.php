74d53929641ab220bf39eddca46ae8ce7567406c
Dattatray <41412142+Dattatrayh@users.noreply.github.com>
2019-01-03 21:36:25 +0530

    Updates bounty.php
    style.css changes,
    bootstrap.min.css changes,
    download.php page created

--- test-web/bounty.php
+++ test-web/bounty.php
@@ -3,17 +3,18 @@ $title = "XinFin: Community Bounty Program";
 $desc = "XinFin Bounty Program, Airdrop for community contributor";
 
 include('inc/header.php') ?>
-<section id="contactbanner"> 
- <!--  <div id="particle-canvas" ></div> -->
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12 col-sm-12 col-xs-12 bannertext">
-      <h1>Bounty Program</h1>
-        <p>XinFin launching a Bounty Program for Community ! Participate in the Bounty Program now. Valid only upto <strong>28th Feb, 2016</strong><br/><br/><br/><br/></p> 
-      </div>
 
-</div></div></div>
-
+<section id="banner" style=" background: url('assets/images/xdc-protocol-banner-image2.png') #112b3c; background-repeat: no-repeat; background-position: bottom right;">
+    <div class="container">
+        <div class="row">
+            <div class="bannertext">
+                <h1>Bounty Program</h1>
+              	<p class="mb-2">XinFin launching a Bounty Program for Community ! Participate in the Bounty Program now. Valid only upto <strong>28th Feb, 2016</strong></p>
+                <p class="mb-2">Get rewarded for contributing code, research, translations and more. Whatever your expertise there is likely work you can help with. Below are a few we need help with now. Sign up to stay up do date with new projects and tasks, announcements, updates and more.</p>
+                <p>Click <a href="http://localhost/Official-XinFinOrg/test-web/resources.php" style="color:#FFFFFF;font-weight:bold;">HERE</a> to learn more about our mission, how the process works, what makes a successful application, different categories of funding, and more.</p>
+            </div>
+        </div>
+    </div>
 </section>
 
 
@@ -24,29 +25,36 @@ include('inc/header.php') ?>
             <div id="network" class="tab-pane fade in active">
                 <div class="row" style="margin-top:0%;padding-bottom:0px;">
                     <div class="col-md-12 col-sm-12 col-xs-12">
-                        <p><strong>Media Coverage Bounty:</strong></p>
-                        <div class="orderList">
+                        <p><strong>PROJECT:</strong></p>
+                        <p><strong>Change of POW Consensus to DPOS in XDC01 Blockchain</strong></p>
+                        <p>Delegated Proof of Stake (DPOS) is the fastest, most efficient, most decentralized, and most flexible consensus model available. DPOS leverages the power of stakeholder approval voting to resolve consensus issues in a fair and democratic way. All network parameters, from fee schedules to block intervals and transaction sizes, can be tuned via elected delegates. Deterministic selection of block producers allows transactions to be confirmed in an average of just 1 second. Perhaps most importantly, the consensus protocol is designed to protect all participants against unwanted regulatory interference.</p>
+                        <p>We are now looking for expanding our protocol development to next level with DPOS based Consensus mechanism from POW (Proof of Work) consensus in blockchain.</p>
+                        
+                        <div class="orderList mb-2">
                         <ol>
-                        	<li> XinFin Coverage with top media like Forbes, Techradar, WSJ, Bloomberg, CNBC, Reuters Bounty: 4.5 Million XDC (Locked for 3 months)</li>
-                        	<li> XinFin "Mention" with top media like Forbes, Techradar, WSJ,Bloomberg, CNBC, Reuters <br />Bounty: 1.5 Million XDC (Locked for 3 months)</li>
-                        	<li> Blog/Medium Article around "XinFin" Bounty : 150K XDC</li>
-                        	<li> Media/Blog / Post/ Video "XinFin" Bounty : 100K XDC</li>
+                        	<li> Registration with KYC</li>
+                        	<li> Block Production by Elected Witnesses</li>
+                        	<li> Double Spend Attack</li>
+                        	<li> Transactions as Proof of Stake</li>
+                            <li> Blockchain Reorganizations</li>
+                            <li> Maximally Decentralized</li>
                         </ol>
-                        <p>Bounty Claim Form: Please add Coverage Media link with Proof of efforts, Mail communication</p>
-                        <p class="link-break-out"><a href="https://docs.google.com/forms/d/1ojBCexSf2Di2EiRS7hZZGSbcBGMI6GbK7K85VIOqL7w/prefil" target="_blank">https://docs.google.com/forms/d/1ojBCexSf2Di2EiRS7hZZGSbcBGMI6GbK7K85VIOqL7w/prefil</a></p>
-                        <p><strong>Technical Development Bounty Program :</strong></p>
-                        <p><strong>Task 1: DPoS Consensus Developm</strong></p>
-                        <p>Too much energy consumption makes PoW consensus non practical and not suitable for real world system. We need to find out other innovative consensus where transaction and network remains de-centralised but without much energy consumption. Restriction numbers of node make its more centralized while large number of nodes makes the system very slow. Many industry experts are trying out different consensus like DPoS. We need to find out innovative solution to replace PoW Consensus with other Consensus which gives more efficiency, speed and less energy consumption. Consensus Related References a. DPOS XDPOS. EOS. TRON. Bounty : 10Million XDC</p>
-                        <p>More detail at: <a href="https://github.com/XinFinOrg/Open-Hackathon" target="_blank">https://github.com/XinFinOrg/Open-Hackathon</a></p>
-                        <p class="link-break-out">Bounty Claim Form: <a href="https://docs.google.com/forms/d/1ojBCexSf2Di2EiRS7hZZGSbcBGMI6GbK7K85VIOqL7w/prefil" target="_blank">https://docs.google.com/forms/d/1ojBCexSf2Di2EiRS7hZZGSbcBGMI6GbK7K85VIOqL7w/prefil</a></p>
-                        <p><strong>Task 2: XDCE Token Staking Function.</strong></p>
-                        <p>A staking mechanism requires users who offer compute power or other resources to the service takers in the ecosystem to put up a portion of their tokens in order to do so. If a user has no tokens to stake, but wants to support the network by providing resources they must purchase them. In exchange for staking their tokens and providing the service a financial reward is received. By forcing providers to take a financial risk you are weeding out bad actors. Read more detailed requirement here DAPPS Related References Boilerplate DAPP. Dapp tutorial.</p>
+                        </div>
+                        
+                        <p><strong>Consensus Related References</strong></p>
+                        
+                        <div class="orderList mb-2">
+                        <ol>
+                        	<li> DPOS</li>
+                            <li> XDPOS</li>
+                            <li> EOS</li>
+                            <li> TRON</li>
+                        </ol>
+                        </div>
+                        
                         <p class="link-break-out">More detail at: <a href="https://github.com/XinFinOrg/Open-Hackathon" target="_blank">https://github.com/XinFinOrg/Open-Hackathon</a></p>
-                        <p class="link-break-out">Bounty Claim Form: <a href="https://docs.google.com/forms/d/1ojBCexSf2Di2EiRS7hZZGSbcBGMI6GbK7K85VIOqL7w/prefil" target="_blank">https://docs.google.com/forms/d/1ojBCexSf2Di2EiRS7hZZGSbcBGMI6GbK7K85VIOqL7w/prefil</a></p>
-                        <p><strong>Task 3 : Cross Blockchain Interoperability.</strong></p>
-                        <p>Various blockchain need to connect to each other to create more efficiency in system. Like Bitcoin, Ripple and IOTA. We need to Create Bridge between various blockchain. Blockchain Interoperability References a. Parity-Bridge. b. BTCRelay.</p>
-                        <p class="link-break-out">Bounty: 1Million XDC More detail at: <a href="https://github.com/XinFinOrg/Open-Hackathon" target="_blank">https://github.com/XinFinOrg/Open-Hackathon</a></p>
-                        <p class="link-break-out">Bounty Claim Form: <a href="https://docs.google.com/forms/d/1ojBCexSf2Di2EiRS7hZZGSbcBGMI6GbK7K85VIOqL7w/prefil" target="_blank">https://docs.google.com/forms/d/1ojBCexSf2Di2EiRS7hZZGSbcBGMI6GbK7K85VIOqL7w/prefil</a></p> 
+                        <p><strong>Value :</strong> </p>
+                        <p><strong>Valid Upto :</strong> </p> 
                     </div>
                 </div>
             </div>
---
+++ test-web/download.php
@@ -0,0 +1,47 @@
+<?php 
+$title = "XinFin: Logo &amp; Brand Asset Downloads";
+$desc = "XinFin Bounty Program, Airdrop for community contributor";
+
+include('inc/header.php') ?>
+<section id="contactbanner" class="download-banner"> 
+ <!--  <div id="particle-canvas" ></div> -->
+	<div class="container">
+		<div class="row">
+		<div class="col-md-12 col-sm-12 col-xs-12 bannertext">
+		<h1>Brand Assets</h1>
+		<p>If you are looking for the XinFin brand assets you are in the right place. The XinFin logo usage outlined below. If you'd prefer to save some time just download the .zip file below.<br/><br/><br/></p>
+      
+		<div>
+			<a  href="http://unbounce-wpengine.netdna-ssl.com/wp-content/themes/unbounce2/images/brand-assets/unbounce-brand-assets.zip" target="_self"><button class="btn-outline-default">DOWNLOAD ALL ASSETS</button></a>
+		</div><br/>
+        <p>XinFin, XDC and XDCe Logos in PNG & EPS formats</p>
+        <br/><br/><br/><br/>
+        
+      </div>
+
+</div></div></div>
+
+</section>
+
+
+
+<section id="protocol">
+	<div class="container">
+        <div class="tab-content">
+            <div id="network" class="tab-pane fade in active">
+                <div class="row" style="margin-top:0%;padding-bottom:0px;">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <h3 class="pb-2">Logo Guidelines</h3>                        
+                        <p class="text-center">The logo should never be placed vertically, outlined or modified in shape or form. Thank you for not embellishing.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+	<div class="clear"></div>
+	</div>
+    </div>
+</section>
+
+
+
+<?php include('inc/footer.php') ?>
