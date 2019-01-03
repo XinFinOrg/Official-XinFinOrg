3fc975108b08638a4d3204385040ca4d9fed42ab
Ritesh kakkad <44856884+riteshkakkad@users.noreply.github.com>
2019-01-03 15:55:10 +0530

    Update pos.php
    PoW page  content added

--- test-web/pos.php
+++ test-web/pos.php
@@ -9,11 +9,95 @@ include('inc/header.php') ?>
     <div class="row">
       <div class="bannertext">
         <h1>Why Proof-of-Stake (PoS) Is Better Than Proof-of-Work (PoW)</h1>
-        <p>Before we move to the point of discussion, understanding the fundamentals of Blockchain technology is essential.Itcame into being when people felt that they do not want to depend ona third-party or intermediary for their transactions. Thus, the evolution of Blockchain technology allowsthe users to conduct their transactions directly with other members without the need of a third-party. That said, a decentralized and distributed system of Blockchain was built that changed the entire landscape of technology, financial, and non-financial transactions in the world.
-          <br/>
-          <br/>
-          In order to make the transactions on Blockchainsecurer and moreaccountable, an effective way or mechanismwas needed that would allow the members to perform mining to reach distributed consensus. Here come, two consensus mechanisms which are named as Proof-of-Work and Proof-of-Stake respectively. These mechanisms are basically the current requirements to confirm that the transactions on a Blockchain are secure and valid.In simple terms, miners place the transactions in a block which after achieving consensus is added to the ever-increasing chain. Hence, the term 'blockchain'.</p>
-      </div>
+        <p><strong>Why Proof-of-Stake (PoS) Is Better Than Proof-of-Work (PoW) </strong></p>
+<p><span style="font-weight: 400;">Before we move to the point of discussion, understanding the fundamentals of Blockchain technology is essential. It came into being when people felt that they do not want to depend on a third-party or intermediary for their transactions. Thus, the evolution of Blockchain technology allows the users to conduct their transactions directly with other members without the need of a third-party. That said, a decentralized and distributed system of Blockchain was built that changed the entire landscape of technology, financial, and non-financial transactions in the world. </span></p>
+<p><span style="font-weight: 400;">In order to make the transactions on Blockchain securer and more accountable, an effective way or mechanism was needed that would allow the members to perform mining to reach distributed consensus. Here come, two consensus mechanisms which are named as Proof-of-Work and Proof-of-Stake respectively. These mechanisms are basically the current requirements to confirm that the transactions on a Blockchain are secure and valid. In simple terms, miners place the transactions in a block which after achieving consensus is added to the ever-increasing chain. Hence, the term &lsquo;blockchain&rsquo;. </span></p>
+<p>&nbsp;</p>
+<h2><strong>Understanding Proof-of-Work</strong></h2>
+<p><span style="font-weight: 400;">Proof-of-Work (PoW) is the first consensus mechanism, generally used by Bitcoin. &nbsp;In this mechanism, the nodes or users perform certain computing to add the block of transactions to the chain. Miners need to calculate a hash (signature) of the block following some particular requirements. Whoever finishes the computational work first will be rewarded with some amount of crypto-currency. This reward compels the users to carry out calculations quickly. Every cryptographic equation is different and unique, ensuring the security protocol of the system. Once the equation is solved, the entire network gets to know that the transaction is valid and authentic. </span></p>
+<p><span style="font-weight: 400;">However, solving the mathematical computation requires a lot of time, efforts, costs, and most importantly, energy consumption. These computational works are quite difficult, eventually requiring heavy rigs for performance. Additionally, there is also a threat for centralisation-by-mining-pool. </span></p>
+<p><span style="font-weight: 400;">This increases the necessity for a better mechanism that doesn&rsquo;t require so much time and money. Something that is faster and much cheaper than Proof-of-Work. </span></p>
+<h2>&nbsp;</h2>
+<h2><strong>How PoS is better than PoW?</strong></h2>
+<p><span style="font-weight: 400;">Firstly, let&rsquo;s understand what PoS is and how it actually works.</span></p>
+<p><span style="font-weight: 400;">A new approach to block generation, PoS is based on the methods of &lsquo;user staking&rsquo;. Like PoW, PoS also maintains the ultimate integrity and security of transactions where users are given incentives to validate other member&rsquo;s transactions. The aim is still the same just like the other blockchain&rsquo;s mechanisms, which is to achieve distributed consensus. However, the difference is to achieve a consensus for a block by placing bets or stakes on it. </span></p>
+<p><span style="font-weight: 400;">Every user can stake an amount of their cryptocurrencies on the block. Then, whoever gets selected to validate the blocks of transaction, gets the reward. Or in other words, when the block is added to the chain, the node who has placed a bet on it will be given reward. The process is completely secure and contains no counterfeit transactions as only an authentic block gets added to the chain. On the other hand, if some node is found with a fraudulent block, it gets penalized and the staked amount would be debited from its balance. </span></p>
+<p><span style="font-weight: 400;">This method entirely depends upon the following idea:</span></p>
+<p><strong><em>&ldquo;Higher stakes in the Blockchain equals to louder voice&rdquo;</em></strong></p>
+<p><span style="font-weight: 400;">The biggest advantages of PoS are given below:</span></p>
+<ol>
+<li style="font-weight: 400;"><span style="font-weight: 400;">Users are not required to spend a lot of time and efforts to perform computations so that they reach a consensus. Instead, they just place a bet on blocks. </span></li>
+<li style="font-weight: 400;"><span style="font-weight: 400;">Electricity costs and excessive energy consumptions are reduced in PoS mechanism.</span></li>
+<li style="font-weight: 400;"><span style="font-weight: 400;">High-performing computers are not needed to stake an amount.</span></li>
+<li style="font-weight: 400;"><span style="font-weight: 400;">A user only needs sufficient amount of tokens to stake and to be eligible to receive rewards. &nbsp;</span></li>
+<li style="font-weight: 400;"><span style="font-weight: 400;">With PoS mechanism, the network becomes fast, inexpensive, and more efficient as compared to PoW. </span></li>
+<li style="font-weight: 400;"><span style="font-weight: 400;">In PoS, every fraudulent activity is penalized. While, in PoW, duplicity can be ignored by other users in the network. This makes PoS a more accountable and secure model. </span></li>
+</ol>
+<p><span style="font-weight: 400;">To get a more vivid idea on why PoS is a better model than PoW, have a look at this comparative representation: </span></p>
+<table>
+<tbody>
+<tr>
+<td>
+<p><strong>Proof Of Stake- PoS</strong></p>
+</td>
+<td>
+<p><strong>Proof of Work - PoW</strong></p>
+</td>
+</tr>
+<tr>
+<td>
+<p><strong>Doesn&rsquo;t require much time and efforts</strong></p>
+</td>
+<td>
+<p><span style="font-weight: 400;">Requires excessive time and efforts</span></p>
+</td>
+</tr>
+<tr>
+<td>
+<p><strong>Doesn&rsquo;t need to solve large mathematical problems </strong></p>
+</td>
+<td>
+<p><span style="font-weight: 400;">Needs to perform heavy calculations </span></p>
+</td>
+</tr>
+<tr>
+<td>
+<p><strong>Requires staking the amount of tokens to get a chance of being selected</strong></p>
+</td>
+<td>
+<p><span style="font-weight: 400;">Requires heavy rigs as difficulty level increases with computational power</span></p>
+</td>
+</tr>
+<tr>
+<td>
+<p><strong>Cheaper than PoW</strong></p>
+</td>
+<td>
+<p><span style="font-weight: 400;">Quite expensive</span></p>
+</td>
+</tr>
+<tr>
+<td>
+<p><strong>Less electricity is consumed resulting in lower energy consumption</strong></p>
+</td>
+<td>
+<p><span style="font-weight: 400;">More electricity is consumed to compute resulting in excessive energy consumption</span></p>
+</td>
+</tr>
+<tr>
+<td>
+<p><strong>Authenticity is encouraged, Unauthenticity is discouraged</strong></p>
+</td>
+<td>
+<p><span style="font-weight: 400;">Authenticity is encouraged but Unauthenticity is forgiven</span></p>
+</td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;</p>
+<p><span style="font-weight: 400;">It is important to note that the users must have enough staking amount to place a bet on the block. The more bets a user places, the higher are their chances of being chosen and getting rewarded. </span></p>
+<p><span style="font-weight: 400;">Now that you know that PoS is far better than PoW, are you ready to invest in Blockchain technology with PoS mechanism? If so, we advise you to opt for </span><a href="https://www.xinfin.org/"><span style="font-weight: 400;">hybrid Blockchain technology</span></a><span style="font-weight: 400;"> &ndash; a game-changing technology consisting of both public and private state of network. It offers robust security and authentic transactions. </span></p>
+<p>&nbsp;</p>      </div>
 
       <!-- <div id="scroll"><a href="#xdce" class="smooth-scroll" ><span class="arrow-bounce">↓</span></a></div> -->
 
