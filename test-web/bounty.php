14ebb3a62ae927ab5cab7a6b2d3a5b451bfabd15
Dattatray <41412142+Dattatrayh@users.noreply.github.com>
2019-01-03 16:33:38 +0530

    Update pos.php
    pos page style updated with style.css

--- test-web/bounty.php
+++ test-web/bounty.php
@@ -1,7 +1,12 @@
-<?php
+<?php 
 $title = "XinFin: Community Bounty Program";
 $desc = "XinFin Bounty Program, Airdrop for community contributor";
 include('inc/header.php') ?>
+<p>
+<p>Bounty Program </p>
+<p>XinFin launching a Bounty Program for Community ! Participate in the Bounty Program now. Valid only upto 28th Feb, 2016</p>
+<p>Media Coverage Bounty : 1.XinFin Coverage with top media like Forbes, Techradar, WSJ,Bloomberg, CNBC, Reuters Bounty: 4.5 Million XDC (Locked for 3 months)</p>
+<p>2. XinFin &rdquo;Mention&rdquo; with top media like Forbes, Techradar, WSJ,Bloomberg, CNBC, Reuters Bounty: 1.5 Million XDC (Locked for 3 months)</p>
 <p><strong>Bounty Program</strong></p>
 <p>XinFin launching a Bounty Program for Community ! Participate in the Bounty Program now. Valid only upto <strong>28th Feb, 2016</strong></p>
 <p><strong>Media Coverage Bounty:</strong></p>
@@ -11,10 +16,19 @@ include('inc/header.php') ?>
 <p>4.Media/Blog / Post/ Video &rdquo;XinFin&rdquo; Bounty : 100K XDC</p>
 <p>Bounty Claim Form: Please add Coverage Media link with Proof of efforts, Mail communication</p>
 <p>https://docs.google.com/forms/d/1ojBCexSf2Di2EiRS7hZZGSbcBGMI6GbK7K85VIOqL7w/prefil</p>
+<p>Technical Development Bounty Program :</p>
+<p>Task 1: DPoS Consensus Developm
+ <p>Too much energy consumption makes PoW consensus non practical and not suitable for real world system. We need to find out other innovative consensus where transaction and network remains de-centralised but without much energy consumption. Restriction numbers of node make its more centralized while large number of nodes makes the system very slow. Many industry experts are trying out different consensus like DPoS. We need to find out innovative solution to replace PoW Consensus with other Consensus which gives more efficiency, speed and less energy consumption. Consensus Related References a. DPOS XDPOS. EOS. TRON. Bounty : 10Million XDC More detail at : https://github.com/XinFinOrg/Open-Hackathon <p>Bounty Claim Form: https://docs.google.com/forms/d/1ojBCexSf2Di2EiRS7hZZGSbcBGMI6GbK7K85VIOqL7w/prefil</p>
+<p>Task 2: XDCE Token Staking Function. A staking mechanism requires users who offer compute power or other resources to the service takers in the ecosystem to put up a portion of their tokens in order to do so. If a user has no tokens to stake, but wants to support the network by providing resources they must purchase them. In exchange for staking their tokens and providing the service a financial reward is received. By forcing providers to take a financial risk you are weeding out bad actors. Read more detailed requirement here DAPPS Related References Boilerplate DAPP. Dapp tutorial. </p>
+More detail at : https://github.com/XinFinOrg/Open-Hackathon
 <p><strong>Technical Development Bounty Program :</strong></p>
 <p>Task 1: <strong>DPoS Consensus Developm</strong></p>
 <p>Too much energy consumption makes PoW consensus non practical and not suitable for real world system. We need to find out other innovative consensus where transaction and network remains de-centralised but without much energy consumption. Restriction numbers of node make its more centralized while large number of nodes makes the system very slow. Many industry experts are trying out different consensus like DPoS. We need to find out innovative solution to replace PoW Consensus with other Consensus which gives more efficiency, speed and less energy consumption. Consensus Related References a. DPOS XDPOS. EOS. TRON. Bounty : 10Million XDC More detail at : https://github.com/XinFinOrg/Open-Hackathon</p>
 <p>Bounty Claim Form: https://docs.google.com/forms/d/1ojBCexSf2Di2EiRS7hZZGSbcBGMI6GbK7K85VIOqL7w/prefil</p>
+<p>Task 3 : Cross Blockchain Interoperability. Various blockchain need to connect to each other to create more efficiency in system. Like Bitcoin, Ripple and IOTA. We need to Create Bridge between various blockchain. Blockchain Interoperability References a. Parity-Bridge. b. BTCRelay.
+<p>Bounty: 1Million XDC More detail at : : https://github.com/XinFinOrg/Open-Hackathon<p>
+ Bounty Claim Form: https://docs.google.com/forms/d/1ojBCexSf2Di2EiRS7hZZGSbcBGMI6GbK7K85VIOqL7w/prefil 
+
 <p><strong>Task 2: XDCE Token Staking Function.</strong></p>
 <p>A staking mechanism requires users who offer compute power or other resources to the service takers in the ecosystem to put up a portion of their tokens in order to do so. If a user has no tokens to stake, but wants to support the network by providing resources they must purchase them. In exchange for staking their tokens and providing the service a financial reward is received. By forcing providers to take a financial risk you are weeding out bad actors. Read more detailed requirement here DAPPS Related References Boilerplate DAPP. Dapp tutorial.</p>
 <p>More detail at : https://github.com/XinFinOrg/Open-Hackathon</p>
