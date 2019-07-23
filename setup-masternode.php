73fb3b4d9a73b8477340d97a0d2fb0550acaea8f
Dattatray <dattatrayh@gmail.com>
2019-07-23 14:51:51 +0530

    Updates bounty.php
    style.css changes

--- setup-masternode.php
+++ setup-masternode.php
@@ -63,7 +63,7 @@ include('inc/header.php') ?>
                         
                         <div class="row mt-5" style="margin-top:0%;padding-bottom:0px;">
                             <div class="col-md-12 col-sm-12 col-xs-12">
-                                <h1>Before You Begin</h1>
+                                <p class="sub-header mt-0 mb-3">Before You Begin</p>
                                 <p>This guide will demonstrate how to set up a single masternode for the very first time. You will need the following before starting:</p>
                                 <Div class="orderList">
                                     <ol>
