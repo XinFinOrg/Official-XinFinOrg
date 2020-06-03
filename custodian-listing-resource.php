064ed32835cfe731052ba9d477a2cef546fc8999
Dattatray <dattatrayh@gmail.com>
2020-06-03 13:05:59 +0530

    Create custodian-listing-resource.php

---
+++ custodian-listing-resource.php
@@ -0,0 +1,290 @@
+<?php 
+$title = "Exchange Listing Resource | Blockchain Technology - XinFin";
+$desc = "Companies and individuals who are looking for real-time cross border financial transactions can use XinFin's Blockchain platform backed integrated wallet solutions.";
+
+include('inc/header.php') ?>
+<link rel='stylesheet' href='assets/css/app-new.css' type='text/css' media='all' />
+
+<section id="contactbanner" style="background: url('assets/images/xdc-protocol-banner-image2.png') #112b3c; background-repeat: no-repeat; background-position: bottom right;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+				<div class="bannertext">
+					<h1>Custodian Listing Resource</h1>
+					<p>Details require to list XinFin-XDC (MainNet Chain) and Xinfin-XDCe with exchange.</p>
+				</div>
+			</div>
+        </div>
+    </div>
+</section>
+
+
+<!--<section id="xdce" class="exchange-listing" style="background: #f3f3f3">-->
+<section id="xdce" class="exchange-listing">
+            
+			<div class="container-fluid">
+			<div class="row">
+				<div class="setup-masternode-row">
+					<!--<div class="container">-->
+						<ul class="nav nav-tabs">
+							<li class="active"><a data-toggle="tab" href="#xinfin-xdc">XDC (MainNet)</a></li>
+							<li><a data-toggle="tab" href="#xinfin-xdce">XDCE</a></li>
+						</ul>
+					<!--</div>-->
+				</div>
+			</div>
+			</div>
+			
+			<div class="container">
+                <div class="tab-content">
+                    
+					<div id="xinfin-xdc" class="tab-pane fade in active">
+					<div id="network">
+                        <div class="row" style="margin-top:0%;padding-bottom:0px;">
+                            <div class="col-md-12">
+	  <p class="header mb-3 mt-0"> Details require to list XinFin-XDC (MainNet Chain) with exchange</p>	  
+	  <div class="row pb-1">
+          <div class="col-md-12">
+          	<p>Project Name: Xinfin Network</p>
+            <p class="link-break-out">Project website: <a href="https://xinfin.org">https://xinfin.org</a></p>
+            <p>Token Name: XDC Mainnet</p>
+            <p>Token Symbol: XDC</p>
+            <p class="link-break-out">Blockchain endpoint: <a href="https://rpc.xinfin.network/" target="_blank">https://rpc.xinfin.network</a></p>
+            <p class="link-break-out">Github: <a href="https://github.com/XinFinOrg/XinFin-Node" target="_blank">https://github.com/XinFinOrg/XinFin-Node</a></p>
+            <p class="link-break-out">Technical White Paper: <a href="https://www.xinfin.org/docs/whitepaper-tech.pdf" target="_blank">https://www.xinfin.org/docs/whitepaper-tech.pdf</a></p>
+            <p class="link-break-out">Business White Paper: <a href="https://www.xinfin.org/docs/whitepaper-business.pdf" target="_blank">https://www.xinfin.org/docs/whitepaper-business.pdf</a></p>
+            <p class="link-break-out">Telegram:  <a href="https://t.me/xinfintalk" target="_blank">https://t.me/xinfintalk</a></p>
+            <p class="link-break-out">Youtube link: <a href="https://www.youtube.com/channel/UCQaL6FixEQ80RJC0B2egX6g" target="_blank">https://www.youtube.com/channel/UCQaL6FixEQ80RJC0B2egX6g</a></p>
+            <p class="link-break-out">Twitter: <a href="https://twitter.com/XinFin_Official" target="_blank">https://twitter.com/XinFin_Official</a></p>
+            <p class="link-break-out">Reddit Link: <a href="https://www.reddit.com/r/xinfin/" target="_blank">https://www.reddit.com/r/xinfin/</a></p>
+            <p class="link-break-out">Slack: <a href="https://xinfin-public.slack.com/" target="_blank">https://xinfin-public.slack.com/</a></p>
+            
+            	<div class="col-md-12">
+                	<div class="row pb-2">
+                    <div style="margin-top:15px;float:left;"><p>XinFin Logo:&nbsp;</p></div>
+                	<div style="float:left;"><img src="assets/images/logox.png" class="img-responsive" style="max-width:50px;"/></div>
+                    </div>
+                </div>
+            	
+                <div class="col-md-12">
+                	<div class="row row pb-2">
+                    <div style="margin-top:15px;float:left;"><p>XDC Logo:</p></div>
+                	<div style="float:left;"><img src="assets/images/exchanges/xdc-logo.png" class="img-responsive" style="max-width:60px;"/></div>
+                    </div>
+                </div>            
+            
+            <p>Pair Reference : XDC:USDC, XDC:ETH, XDC:BTC,  XDC:USD</p>
+            <p>Preference to run bounty plan : No</p>
+			
+			<p class="sub-header mt-5">How to create account in XinFin Blockchain?</p>
+			<p class="mt-1 mb-1"><strong>1. Using Keystore</strong></p>
+			<div style="margin-left:20px;">
+				<pre><code>web3.eth.personal.newAccount(password, [callback])</code></pre>
+				
+				<p class="mt2 mb-1"><strong>Parameters</strong></p>
+				<p><code>password - String:</code> The password to encrypt this account with.</p>
+				
+				<p class="mt-2 mb-1"><strong>Returns</strong></p>
+				<p><code>Promise</code> returns <code>String:</code> The address of the newly created account.</p>
+				
+				<p class="mt-2 mb-1"><strong>Example</strong></p>
+				<pre><code>web3.eth.personal.newAccount('!@superpassword')
+.then(console.log);
+> '0x1234567891011121314151617181920212223456'</code></pre>
+			</div>
+			
+			<p class="mt-3 mb-1"><strong>2. Using Private key</strong> (Generates an account object with private key and public key.)</p>
+			<div style="margin-left:20px;">
+				<pre><code>web3.eth.accounts.create([entropy]);</code></pre>
+				
+				<p class="mt-2 mb-1"><strong>Parameters</strong></p>
+				<p><code>entropy - String</code> (optional): A random string to increase entropy. If given it should be at least 32 characters. If none is given a random string will be generated using random hex.</p>
+				
+				<p class="mt-2 mb-1"><strong>Returns</strong></p>
+				<p><code>Object</code> - The account object with the following structure:</p>
+				<ul style="margin-left:20px;">
+					<li><code>address - string</code> : The account address.</li>
+					<li><code>privateKey - string</code> : The accounts private key. This should never be shared or stored unencrypted in localstorage! Also make sure to null the memory after usage.</li>
+					<li><code>signTransaction(tx [, callback]) - Function</code> : The function to sign transactions. See <a href="https://web3js.readthedocs.io/en/v1.2.0/web3-eth-accounts.html#eth-accounts-signtransaction" target="_blank">web3.eth.accounts.signTransaction()</a> for more.</li>
+					<li><code>sign(data) - Function</code> : The function to sign transactions. See <a href="https://web3js.readthedocs.io/en/v1.2.0/web3-eth-accounts.html#eth-accounts-sign" target="_blank">web3.eth.accounts.sign()</a> for more.</li>
+				</ul>
+				
+				<p class="mt-2 mb-1"><strong>Example</strong></p>
+				<pre><code>web3.eth.accounts.create();
+> {
+    address: "0xb8CE9ab6943e0eCED004cDe8e3bBed6568B2Fa01",
+    privateKey: "0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709",
+    signTransaction: function(tx){...},
+    sign: function(data){...},
+    encrypt: function(password){...}
+}</code></pre>
+			</div>
+			
+			<p class="sub-header mt-5">How to deposit XDC?</p>
+			<p class="mt-1 mb-1"><strong>1. New Block Notification</strong></p>
+				
+			<p class="mt-1 mb-1"><strong>2. getTransactionFromBlock</strong></p>
+			<pre><code>getTransactionFromBlock(hashStringOrNumber, indexNumber [, callback])</code></pre>
+			<p>Returns a transaction based on a block hash or number and the transactions index position.</p>
+			
+			<div style="margin-left:20px;">
+				<p class="mt-2 mb-1"><strong>Parameters</strong></p>
+				<ul style="margin-left:20px;">
+					<li><code>string</code> - A block number or hash. Or the string <code>"genesis", "latest"</code> or <code>"pending"</code> as in the <a href="https://web3js.readthedocs.io/en/v1.2.0/web3-eth.html#eth-defaultblock" target="_blank">default block parameter</a>.</li>
+					<li><code>Number</code> - The transactions index position.</li>
+					<li><code>Function</code> - (optional) Optional callback, returns an error object as first parameter and the result as second.</li>
+					<li><code>sign(data) - Function</code> : The function to sign transactions. See web3.eth.accounts.sign() for more.</li>
+				</ul>
+				
+				<p class="mt-2 mb-1"><strong>Returns</strong></p>
+				<p><code>Promise</code> returns <code>Object</code> - A transaction object.</p>
+				
+				<p class="mt-2 mb-1"><strong>Example</strong></p>
+				<pre><code>var transaction = web3.eth.getTransactionFromBlock('0x4534534534', 2)
+.then(console.log);
+> > {
+    "hash": "0x9fc76417374aa880d4449a1f7f31ec597f00b1f6f3dd2d66f4c9c6c445836d8b",
+    "nonce": 2,
+    "blockHash": "0xef95f2f1ed3ca60b048b4bf67cde2195961e0bba6f70bcbea9a2c4e133e34b46",
+    "blockNumber": 3,
+    "transactionIndex": 0,
+    "from": "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b",
+    "to": "0x6295ee1b4f6dd65047762f924ecd367c17eabf8f",
+    "value": '123450000000000000',
+    "gas": 314159,
+    "gasPrice": '2000000000000',
+    "input": "0x57cb2fc4"
+}</code></pre>
+			</div>
+			
+			<p class="sub-header mt-5">How to withdraw XDC?</p>
+			<p class="mt-1 mb-1"><strong>1. sendTransaction</strong></p>
+			<pre><code>web3.eth.sendTransaction(transactionObject [, callback])</code></pre>
+			<p>Sends a transaction to the network.</p>
+			
+			<div style="margin-left:20px;">
+				<p class="mt-2 mb-1"><strong>Parameters</strong></p>
+				<p>1. <code>Object</code> - The transaction object to send:</p>
+				<ul style="margin-left:30px;">
+					<li><code>from - String|Number</code> : The address for the sending account. Uses the web3.eth.defaultAccount property, if not specified. Or an address or index of a local wallet in web3.eth.accounts.wallet.</li>
+					<li><code>to - String</code> : (optional) The destination address of the message, left undefined for a contract-creation transaction.</li>
+					<li><code>value - Number|String|BN|BigNumber</code> : (optional) The value transferred for the transaction in wei, also the endowment if it's a contract-creation transaction.</li>
+					<li><code>gas - Number</code> : (optional, default: To-Be-Determined) The amount of gas to use for the transaction (unused gas is refunded).</li>
+					<li><code>gasPrice - Number|String|BN|BigNumber</code> : (optional) The price of gas for this transaction in wei, defaults to web3.eth.gasPrice.</li>
+					<li><code>data - String</code> : (optional) Either a ABI byte string containing the data of the function call on a contract, or in the case of a contract-creation transaction the initialisation code.</li>
+					<li><code>nonce - Number</code> : (optional) Integer of a nonce. This allows to overwrite your own pending transactions that use the same nonce.</li>	
+				</ul>
+				<p>2. <code>callback - Function</code> : (optional) Optional callback, returns an error object as first parameter and the result as second.</p>
+				
+				<p class="mt-3 mb-1"><strong>Returns</strong></p>
+				<p>The <strong>callback</strong> will return the 32 bytes transaction hash.</p>
+				<p><code>PromiEvent</code> : A <a href="https://web3js.readthedocs.io/en/v1.2.0/callbacks-promises-events.html#promievent" target="_blank">promise combined event emitter</a>. Will be resolved when the transaction <a href="https://web3js.readthedocs.io/en/v1.2.0/web3-eth.html#eth-gettransactionreceipt-return" target="_blank">receipt</a> is available. Additionally the following events are available:</p>
+				<ul style="margin-left:30px;">
+					<li><code>"transactionHash"</code> returns <code>String</code> : Is fired right after the transaction is sent and a transaction hash is available.</li>
+					<li><code>"receipt"</code> returns <code>object</code> : Is fired when the transaction receipt is available.</li>
+					<li><code>"confirmation"</code> returns <code>Number, Object</code> : Is fired for every confirmation up to the 12th confirmation. Receives the confirmation number as the first and the <a href="https://web3js.readthedocs.io/en/v1.2.0/web3-eth.html#eth-gettransactionreceipt-return" target="_blank">receipt</a> as the second argument. Fired from confirmation 0 on, which is the block where its minded.</li>
+					<li><code>"error"</code> returns <code>Error</code> : Is fired if an error occurs during sending. If a out of gas error, the second parameter is the receipt.</li>					
+				</ul>
+				
+				<p class="mt-3 mb-1"><strong>Example</strong></p>
+				<pre><code>// compiled solidity source code using https://remix.ethereum.org
+var code = "603d80600c6000396000f3007c01000000000000000000000000000000000000000000000000000000006000350463c6888fa18114602d57005b6007600435028060005260206000f3";
+// using the callback
+web3.eth.sendTransaction({
+    from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe',
+    data: code // deploying a contracrt
+}, function(error, hash){
+    ...
+});
+// using the promise
+web3.eth.sendTransaction({
+    from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe',
+    to: '0x11f4d0A3c12e86B4b5F39B213F7E19D048276DAe',
+    value: '1000000000000000'
+})
+.then(function(receipt){
+    ...
+});
+// using the event emitter
+web3.eth.sendTransaction({
+    from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe',
+    to: '0x11f4d0A3c12e86B4b5F39B213F7E19D048276DAe',
+    value: '1000000000000000'
+})
+.on('transactionHash', function(hash){
+    ...
+})
+.on('receipt', function(receipt){
+    ...
+})
+.on('confirmation', function(confirmationNumber, receipt){ ... })
+.on('error', console.error); // If a out of gas error, the second parameter is the receipt.</code></pre>
+			</div>			
+
+         </div>
+      </div>							</div>
+                        </div>
+                    </div>
+					</div>
+					
+					<div id="xinfin-xdce" class="tab-pane fade">
+					<div id="network">
+                        <div class="row" style="margin-top:0%;padding-bottom:0px;">
+							<div class="col-md-12">
+      <p class="header mb-3 mt-0">Details require to list Xinfin-XDCe with exchange</p>
+      <div class="row pb-1">
+          <div class="col-md-12">
+          	<p>Project Name: Xinfin Network</p>
+            <p class="link-break-out">Project website: <a href="https://xinfin.org">https://xinfin.org</a></p>
+            <p class="link-break-out">Contract Address: 0x41AB1b6fcbB2fA9DCEd81aCbdeC13Ea6315F2Bf2</p>
+            <p>Token Name: XinFin Network</p>
+            <p>Token Symbol: XDCE</p>
+            <p>Decimal: 18</p>
+            <p class="link-break-out">Github: <a href="https://github.com/XinFinorg/xdce" target="_blank">https://github.com/XinFinorg/xdce</a></p>
+            <p class="link-break-out">Technical White Paper: <a href="https://www.xinfin.org/docs/whitepaper-tech.pdf" target="_blank">https://www.xinfin.org/docs/whitepaper-tech.pdf</a></p>
+            <p class="link-break-out">Business White Paper: <a href="https://www.xinfin.org/docs/whitepaper-business.pdf" target="_blank">https://www.xinfin.org/docs/whitepaper-business.pdf</a></p>
+            <p class="link-break-out">Telegram:  <a href="https://t.me/xinfintalk" target="_blank">https://t.me/xinfintalk</a></p>
+            <p class="link-break-out">Youtube link: <a href="https://www.youtube.com/channel/UCQaL6FixEQ80RJC0B2egX6g" target="_blank">https://www.youtube.com/channel/UCQaL6FixEQ80RJC0B2egX6g</a></p>
+            <p class="link-break-out">Coinmarketcap: <a href="https://coinmarketcap.com/currencies/xinfin-network/" target="_blank">https://coinmarketcap.com/currencies/xinfin-network/</a></p>
+            <p class="link-break-out">Twitter: <a href="https://twitter.com/xinfinf" target="_blank">https://twitter.com/xinfinf</a></p>
+            <p class="link-break-out">Reddit Link: <a href="https://www.reddit.com/r/xinfin/" target="_blank">https://www.reddit.com/r/xinfin/</a></p>
+            <p class="link-break-out">Slack: <a href="https://xinfin-public.slack.com/" target="_blank">https://xinfin-public.slack.com/</a></p>
+            <p class="link-break-out">Etherscan link: <a href="https://etherscan.io/token/0x41ab1b6fcbb2fa9dced81acbdec13ea6315f2bf2?a=" target="_blank">https://etherscan.io/token/0x41ab1b6fcbb2fa9dced81acbdec13ea6315f2bf2?a=</a></p>            
+            
+            	<div class="col-md-12">
+                	<div class="row pb-2">
+                    <div style="margin-top:15px;float:left;"><p>XinFin Logo:&nbsp;</p></div>
+                	<div style="float:left;"><img src="assets/images/logox.png" class="img-responsive" style="max-width:50px;"/></div>
+                    </div>
+                </div>
+            	
+                <div class="col-md-12">
+                	<div class="row row pb-2">
+                    <div style="margin-top:15px;float:left;"><p>XDCe Logo:</p></div>
+                	<div style="float:left;"><img src="assets/images/exchanges/xdce-logo.png" class="img-responsive" style="max-width:60px;"/></div>
+                    </div>
+                </div>            
+            
+            <p>Pair Reference : XDCe:USDC, XDCe:ETH, XDCe:BTC, XDCe:Eurp, XDCe:USD</p>
+            <p>Preference to run bounty plan : No</p>
+          </div>
+      </div>
+							</div>
+						</div>                        
+                    </div>
+					</div>										
+                </div>
+            </div>
+            </div>
+            </div>
+            <div class="clear"></div>
+        </section>
+
+
+
+<script>
+    var activeUrl = localStorage.getItem("currentUrl");
+    $('.nav.navbar-nav li:nth-child(5)').addClass('active');
+</script>
+
+<?php include('inc/footer.php') ?>
