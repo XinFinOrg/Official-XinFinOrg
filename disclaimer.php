70ff5b647ae96677dd5dca670395d5491c4d950a
Dattatray <dattatrayh@gmail.com>
2021-10-21 16:56:28 +0530

    disclaimer and privacy page updates

--- disclaimer.php
+++ disclaimer.php
@@ -7,8 +7,8 @@ include('inc/header.php') ?>
 <!--   <div id="particle-canvas" ></div> -->
   <div class="container">
     <div class="row">
-      <div class="col-md-12 col-sm-12 col-xs-12 bannertext"><h1>Disclaimer</h1>
-         <p>The website https://www.xinfin.org (hereinafter, referred to as the “Website”) provides information and material of a general nature. You are not authorized and nor should you rely on the Website for legal advice, business advice, or advice of any kind. You act at your own risk in reliance on the contents of the Website.<br/><br/><br/><br/></p> 
+      <div class="col-md-12 col-sm-12 col-xs-12 bannertext"><h1>Disclaimer and Terms of Use</h1>
+         <p class="mb-3">&nbsp;</p> 
       </div>
       <!-- <div class="col-md-6 col-sm-6 col-xs-12 btxt-img">
         <img src="assets/images/XDC-Protocol/xdc-protocol-banner-image.png" alt="" class="img-responsive">
@@ -32,54 +32,45 @@ include('inc/header.php') ?>
       
       <div class="row" style="margin-top: 0%;">
         <div class="col-md-12 col-sm-12 col-xs-12">
-          <p>
-          <strong>Information published on www.xinfin.org</strong><br/><br/>
-The website https://www.xinfin.org (hereinafter, referred to as the “Website”) provides information and material of a general nature. You are not authorized and nor should you rely on the Website for legal advice, business advice, or advice of any kind. You act at your own risk in reliance on the contents of the Website. In no way is the management of website or the contributors to the Website are responsible for the actions, decisions, or other behavior taken or not taken by you in reliance upon the Website.
-<br/><br/>
-<strong>Forward Looking Statements</strong><br/><br/>
-The information comprised on Website herein contains forward-looking statements and involves, but is not limited to, the (i) proposed performance of the XDC tokens; (ii) the anticipated development of the Company’s business; (iii) completion of the Company’s projects that are currently underway, in development or otherwise, under consideration. Forward-looking statements are produced to enable potential investors the opportunity to comprehend the beliefs & opinions of management in respect of future, so that, they can use such beliefs & opinions as an element in gauging an investment.
-<br/><br/>
-The given statements, under no circumstance, are guarantees of upcoming performance and undue reliance must not be placed on them. These forward-looking statements certainly include known and unknown risks & uncertainties that may result in actual performance and financial outcomes in future to vary materially from any evaluation of future performance and/or result expressed or implied by these forward-looking statements.
-<br/><br/>
-The site does not undertake any obligation to update such forward-looking statements. No part of this disclaimer may be reproduced, transmitted or transferred in any form or by any means, electronic or mechanical, involving recording, photocopying, or information storage and retrieval systems or translated to other language without the express written permission of XinFin.
-<br/><br/>
-<strong>Risks related to use of XDC tokens</strong><br/><br/>
-The XDC tokens, powered by Xinfin’s XDC01 protocol are private investment instruments and are not registered with any regulatory agency of any jurisdiction, and are NOT subject to the same regulatory needs as SEC-registered exchange traded funds or mutual funds, involving the need to offer certain periodic and standardized pricing and valuation information to investors. The XDC tokens have not been reviewed or/and approved by federal, state or local regulators of any jurisdiction.
-<br/><br/>
-You should carefully note the following:
-<br/><br/>
-The XDC token serves as a speculative investment and includes a high degree of risk. Investors can lose all or a considerable portion of their investments. Anyone planning to invest must have sophistication/experience, financial ability, and willingness to bear risks of an investment in the XDC tokens.<br/><br/>
-The XDC token’s fees and expenses (which may be considerable, regardless of any returns on investment) will offset the XDC token’s trading profits.<br/><br/>
-Xinfin, its managers, advisors or associates may rely on trading expertise and experience of 3rd party managers or advisors, the identity of which may not be completely disclosed to investors.
-The XDC and its sponsors, advisors, managers, and agents may be subject to several conflicts of interest.
-The aforementioned summary is not a full list of risks and other necessary disclosures, included in investing in the XDC token and is subject to more complete disclosures constituted in the XDC References on website, which should be carefully reviewed.
-<br/><br/>
-<strong>Investment Risks</strong><br/><br/>
-The price of XDC tokens could be highly volatile and the investors in XDC should expect prices to have wide range fluctuations. The investment in XDC can lead to loss of money over short or even long periods. Please note that cryptocurrency trading may not be appropriate for all the users of this Website. Anyone who is planning to invest in cryptocurrencies should consult a well-qualified, independent, and professional financial advisor. The information published on the Website cannot guarantee that the investors in XDC would not lose money.
-<br/><br/>
-<strong>No Investment Advice</strong><br/><br/>
-The entire information given on the site does not consist of investment advice, trading advice, financial advice and/or any other kind of advice and, you under no circumstance, should take any/all of the site’s content in such manner. XinFin never recommends that you should buy, sell or hold any XDC token and nothing on the site; should be considered as a proposal to buy, sell or hold the XDC token/tokens. Do carry you own due diligence and consult a qualified & professional financial advisor before you take any such investment decisions.
-<br/><br/>
-<strong>No warranties</strong><br/><br/>
-The Website is provided on an “as is” basis without any warranties of any kind regarding the Website and/or any content, data, materials and/or services provided on the Website.
-<br/><br/>
-<strong>Limitation of liability</strong><br/><br/>
-In no event shall XinFin, its MANAGEMENT, EMPLOYEES OR ASSOCIATES be liable for any direct, indirect, special or consequential damages of any kind, including, but not limited to, loss of use, loss of profits, or loss of data arising out of or in any way connected with the use of the Website or XDC tokens.
-<br/><br/>
-<strong>Accuracy of Information</strong><br/><br/>
-The website, in all scenarios, will strive to assure that all the information present on the site is accurate, although, it holds no responsibility of any wrong or missing information. You completely comprehend that you are utilizing any and/or all information present on this site at your very own risk.
-<br/><br/>
-<strong>Modifications to the site</strong><br/><br/>
-XinFin reserves the right to change or modify the terms and conditions contained in these Terms or any Policy or Guideline of the Site, at any time and in its sole discretion. We will provide notice of these changes by posting the revised Terms to the Site and changing the “Last Amendment” date at the top of the Terms, or by providing other means of notice as XinFin will determine each time in its sole discretion. Any changes or modifications will be effective immediately upon posting the revisions to the Site, and will apply to your subsequent use of the Site. Your continued use of this Site will confirm your acceptance of such changes or modifications; therefore, you should review the Terms and applicable policies whenever you use the Site to understand the terms that apply to such use.
-<br/><br/>
-<strong>Links to the other sites</strong><br/><br/>
-The website might contain links to other websites which are not managed by XinFin. The features, subject and accessibility of those sites are not handled by XinFin. Any such links included will not imply approval or ratifying the content mentioned therein.
-<br/><br/>
-<strong>Website Downtime & Maintenance</strong><br/><br/>
-XinFin is determined that website is working efficiently all the time. But, XinFin does not take the obligation and is also not liable for the website being unavailable at some point of time due to technical issues that are not in our control.
-<br/><br/>
-<strong>Amendment</strong><br/><br/>
-This disclaimer was amended for the last time on September 27th, 2017.</p>
+          <p class="mb-1"><strong>Information Published on the Website</strong></p>
+          <p>This website at www.xinfin.org (hereinafter referred to as the "Website") is published and maintained by XDC Foundation to provide information and material related to the XDC Network and associated XDC coin. You are not authorized to, nor should you, rely on the Website for legal advice, business advice, investment advice, technical advice, or advice of any kind whatsoever. You should not place any reliance on the information provided on the Website; you alone bear the full risk of any action taken or refrained from in reliance on the Website or its contents.</p>
+          
+          <p class="mb-1"><strong>Forward-looking statements</strong></p>
+          <p>The information on the Website may contain forward-looking statements including, but not limited to, (i) the anticipated or supposed future performance of the XDC coin; (ii) the anticipated development of dApps or other functionality on the XDC Network; (iii) completion of projects that are currently underway, in development or otherwise under consideration for implementation on or in conjunction with the XDC Network. Forward-looking statements are produced only for the purpose of promoting a decentralized, informed, and robust community and not to enable potential investors to gauge the future viability or profitability of building or investing in relation to the XDC Network or the XDC coin.</p>
+          <p>Under no circumstances are any statements on the Website intended or otherwise to be construed as guarantees of upcoming performance.  No reliance should be placed on any statement or information on the Website.  The actual future performance and financial outcomes associated with the XDC Network and the XDC coin may and likely will vary materially from any evaluation of future performance or result expressed or implied by any forward-looking statements on the Website.</p>
+          <p>No obligation is undertaken by the XDC Foundation or by any member of the community to update any forward-looking statements or to otherwise ensure a likelihood that the statements will ultimately correlate to reality.</p>
+          
+          <p class="mb-1"><strong>Risks related to use of XDC coins</strong></p>
+          <p>XDC coins and tokens, powered by XDC Network's XDPoS consensus protocol, are private instruments, are not registered with any regulatory agency of any jurisdiction, and are not subject to the same regulatory requirements as exchange traded funds or mutual funds which would be registered with appropriate regulatory authorities (e.g., the Monetary Authority of Singapore or the Securities and Exchange Commission in the United States).  Because XDC coins are not registered instruments, periodic and standardized pricing and valuation information is not offered to investors on the Website or otherwise. XDC coin have not been reviewed or approved by federal, state or local regulators of any jurisdiction.</p>
+          <p>You should carefully note the following:</p>
+          <p>To the extent any person would purchase or receive XDC coin for investment purposes, such investment is purely speculative investment and involves a high degree of risk. Any investor can lose all or a considerable portion of his or her investment, but this is particularly true with an investment that is unregulated. Anyone planning to invest must have appropriate sophistication, experience, financial ability, and willingness to bear risks of an investment.  No investment in XDC coins should be made in an amount greater than the maximum loss you are willing and able to sustain.</p>
+          <p>Fees and expenses associated with transfer of XDC coins (which may be considerable, regardless of any other factors) will offset any trading profits that might be realized through the purchase, sale, and exchange of XDC coin.</p>
+          <p>In making statements on the Website, XDC Foundation, its directors, advisors or associates, and community members may rely on trading expertise and experience of third-party advisors, the identity of which may not be disclosed to the public. XDC Foundation, its sponsors, directors, advisors, and agents may have conflicts of interest.</p>
+          <p>These few risks described here are not a complete list of risks associated with reliance on the information set forth on the Website.  Other disclosures, which would be deemed necessary in order to make a knowing and informed  investment, may be available on other websites or from other sources; such sources ought to be carefully reviewed prior to purchasing XDC coins or tokens or otherwise incurring any risk associated therewith.</p>
+          
+          <p class="mb-1"><strong>Investment Risks</strong></p>
+          <p>The price of XDC coins and tokens are highly volatile and you should expect prices to have widely ranging fluctuations. Any investment in XDC can lead to loss of money over short or long periods. "Cryptocurrency" trading is not appropriate for all users of the Website. Anyone who is planning to invest in cryptocurrencies should consult a well-qualified, independent, and professional financial advisor. The information published on the Website is not a suitable replacement for such an advisor and cannot guarantee that a purchaser of XDC will not lose money.</p>
+          
+          <p class="mb-1"><strong>No Investment Advice</strong></p>
+          <p>The entire body of information given on the Website does not consist of or include investment advice, trading advice, financial advice, technical advice, or any other kind of advice.  Under no circumstance should you take any of the Website's content as advice. XDC Foundation never recommends that you buy, sell or hold any XDC coins or tokens.  Nothing on the website should be considered as a proposal or recommendation that you buy, sell or hold any XDC coins or tokens. The closest things to advice we can offer you is to emphatically state that you should fully conduct your own due diligence and consult a qualified professional financial advisor before you take any investment decisions.</p>
+          
+          <p class="mb-1"><strong>No warranties</strong></p>
+          <p>The Website is provided on an "as is" basis without any warranties of any kind regarding the Website or any content, data, material, information, or services provided on the Website.</p>
+          
+          <p class="mb-1"><strong>Limitation of liability</strong></p>
+          <p>In no event shall the XDC Foundation, its Directors, or employees, or any community members be liable for any direct, indirect, special or consequential damages of any kind, including, but not limited to, loss of use, loss of profits, or loss of data arising out of or in any way connected with the use of the Website or XDC coins.</p>
+          
+          <p class="mb-1"><strong>Accuracy of Information</strong></p>
+          <p>Given the rapidity with which blockchain is developing, the fact that change is the only constant in the cryptosphere, and the intentional efforts to allow and encourage participation by the community at large, XDC Foundation makes no general or specific assurances of accuracy with respect to the Website or any information on it.  XDC Foundation holds no responsibility of any wrong, misleading, outdated, or missing information. By using the Website, you represent that you are utilizing any and all information present on the Website at your own risk.</p>
+          
+          <p class="mb-1"><strong>Modifications to the site</strong></p>
+          <p>XDC Foundation reserves the right to change or modify the terms and conditions contained in these terms, any policy or guideline of the Website, and the content of the Website at any time and in its sole discretion. The only notice of such changes will be by posting the revised terms, policy, guideline, or content to the Website. Any changes or modifications will be effective immediately upon posting the revisions to the Website and will apply to all subsequent use of the Website. Your continued use of the Website will confirm your acceptance of such changes or modifications; therefore, you should review the terms and applicable policies whenever you use the Website to understand the terms that apply to such use.</p>
+          
+          <p class="mb-1"><strong>Links to the other sites</strong></p>
+          <p>The Website may contain links to other websites which are not managed by XDC Foundation. The features, content and accessibility of those sites are not controlled by XDC Foundation. Any such links included do not imply approval or endorsement of the content included in the linked sites.</p>
+          
+          <p class="mb-1"><strong>Website Downtime & Maintenance</strong></p>
+          <p>At various and unpredictable times, the Website will be down for maintenance, updating, or for reasons not yet anticipated.  XDC Foundation is not liable to you for damages, difficulties, or inconvenience caused by the unavailability of the Website regardless of the cause of the unavailability.</p>
         </div>
         
       </div>
