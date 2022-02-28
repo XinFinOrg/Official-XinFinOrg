da3754fa82457cd8184ec10f27cffcd0fb713b6a
Dattatray Hande <dattatrayh@gmail.com>
2022-02-28 15:01:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -586,10 +586,10 @@ include('inc/header.php') ?>
                     <div class="col-md-2 col-sm-4 col-xs-6 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s">
                         <a href="https://www.marketwatch.com/press-release/xinfin-network-secures-ex-swift-corporate-trade-head-andre-casterman-as-an-advisor-2020-07-16" target="_blank"><img src="assets/images/home/media/marketwatch-logo.png" alt="MarketWatch" class="img-responsive margin-auto-lr"></a>
                     </div>
-					<div class="col-md-2 col-sm-4 col-xs-6 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s">
+					<!--<div class="col-md-2 col-sm-4 col-xs-6 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s">
                         <a href="https://in.finance.yahoo.com/news/buyucoin-becomes-first-indian-cryptocurrency-053950377.html" target="_blank"><img src="assets/images/home/media/in-yahoofinance-logo.png" alt="Yahoo Finance" class="img-responsive margin-auto-lr"></a>
                     </div>
-					<!--<div class="col-md-2 col-sm-4 col-xs-6 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s">
+					<div class="col-md-2 col-sm-4 col-xs-6 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.6s">
                         <a href="http://forbes.com/sites/lukefitzpatrick/2020/05/24/spending-on-blockchain-solutions-could-surpass-16-billion-by-2023/" target="_blank"><img src="assets/images/home/media/forbes-logo.png" alt="Forbes" class="img-responsive margin-auto-lr"></a>
                     </div>-->					
 					<!--<div class="col-md-2 col-sm-4 col-xs-6 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s">
