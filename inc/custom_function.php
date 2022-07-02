6fbb583e4338714d1010680f1a6eee02d7c5551a
Dattatray Hande <dattatrayh@gmail.com>
2022-07-02 13:35:42 +0530

    Update custom_function.php

--- inc/custom_function.php
+++ inc/custom_function.php
@@ -2,7 +2,8 @@
 
 	function base_url(){
 
-		$base_url = 'http://localhost/xinfin_new/';
+		//$base_url = 'http://localhost/xinfin_new/';
+		$base_url = 'https://xinfin.org/';
 		//$base_url = 'http://localhost/Official-XinFinOrg';
 		return $base_url;
 	}
