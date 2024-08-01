6b8b535bf48b96f50784a6860c414a39ddd06ca6
Dattatray Hande <dattatrayh@gmail.com>
2024-08-01 18:41:50 +0530

    xdc-card.php page added

---
+++ xdc-card.php
@@ -0,0 +1,33 @@
+<?php 
+    $title = "XDC Chain Network Tools and Documents";
+    $desc = "Access network tools and documents for XDC Chain Network";
+    
+    include('inc/header.php') ?>
+
+<!-- Hero Home Starts -->
+<section class="px-60 hero-inside">
+    <!--<div class="container p-relative">-->
+    <div class="container">
+        
+    </div>
+</section>
+<!-- Hero Home Ends -->
+
+
+
+<!-- Other Tools Starts -->
+<section class="px-60 pt-1 bg-lightgray">
+    <div class="container">        
+        <div class="row mt-5 align-items-center">
+			<div class="col-xl-12 col-12">
+            	<div><img src="assets/images/xdc-card.gif" alt="XDC Index — Vinter" class="img-fluid br-20" /></div>
+        	</div>
+        </div>
+                        
+                        
+    </div>
+</section>
+
+
+
+<?php include('inc/footer.php') ?>
