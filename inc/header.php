b507a109c5ae156170a651c281884f19e5a98fa6
Dattatray Hande <dattatrayh@gmail.com>
2024-10-11 12:53:36 +0530

    Update events.php
    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -74,8 +74,8 @@
         <header class="autohide header-wrapper sticky-header">
             <div class="news-bar">
                 <p>
-                    Join XDC Network at <b>HONG KONG FINTECH WEEK'24</b> : 28 - 29 October, 2024.
-                    <a href="https://www.fintechweek.hk/" target="_blank" class="news-bar-button">
+                    Join XDC Network at <b>GITEX GLOBAL</b> : 14 - 18 October, 2024.
+                    <a href="https://www.gitex.com/" target="_blank" class="news-bar-button">
                         JOIN US
                         <svg class="w-6 h-6 icon ml-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
