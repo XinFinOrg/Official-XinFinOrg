eb56a5b93c578a8a7f7f14ae57da21c8f841bb63
Dattatray <dattatrayh@gmail.com>
2025-02-12 11:50:30 +0530

    Update xdc-accelerator-program.php

--- xdc-accelerator-program.php
+++ xdc-accelerator-program.php
@@ -265,12 +265,13 @@
                             <p class="aDate fs-6 mb-0"><span class="fw-600">Application Deadline:</span> 10 August 2024</p>
                         </div>
 
-                        <a href="https://launchpoollabs.xyz/#submit" target="_blank" class="btn socialHead-button internalLink mt-auto">
+                        <h2 class="closedTag">Applications Closed!</h2>
+                        <!--<a href="https://launchpoollabs.xyz/#submit" target="_blank" class="btn socialHead-button internalLink mt-auto">
                             Apply Now
                             <svg class="w-6 h-6 icon ml-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
                             </svg>
-                        </a>
+                        </a>-->
                     </div>
                 </div>
             </div>                
