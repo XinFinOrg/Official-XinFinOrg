e3a9162001d7af50d0387958881da602db5f398d
Dattatray <dattatrayh@gmail.com>
2026-06-01 15:42:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -502,7 +502,7 @@
                     <div class="card-info">
                         <p class="fw-500">Transactions</p>
                         <div class="cardHead fw-600 lh-1">
-                            <span class="counter-api counter" id="transactionCount">983.95 </span>
+                            <span class="counter-api counter" id="transactionCount">1,080.05 </span>
                             <span id="transactionText">M</span>
                         </div>
                         <div class="statsIcon">
@@ -516,7 +516,7 @@
                     <div class="card-info">
                         <p class="fw-500">Contracts</p>
                         <div class="cardHead fw-600 lh-1">
-                            <span class="counter-api counter" id="contractNumber">676,370</span>
+                            <span class="counter-api counter" id="contractNumber">742,357</span>
                             <span id="contractNumberText"></span>
                         </div>
                         <div class="statsIcon">
@@ -529,7 +529,7 @@
                 <div class="card overflow-hidden pt-4 pb-4 px-3 bg-lightgray br-20 border-0">
                     <div class="card-info">
                         <p class="fw-500">Validators/Total Nodes</p>
-                        <div class="cardHead fw-600 lh-1"><span class="counter-api counter" id="masternodes">108</span>/<span class="counter-api counter" id="totalnodes">241</span></div>
+                        <div class="cardHead fw-600 lh-1"><span class="counter-api counter" id="masternodes">108</span>/<span class="counter-api counter" id="totalnodes">257</span></div>
                         <div class="statsIcon">
                             <img src="assets/images/icons/validators-nodes.png" class="img-fluid" alt="XDC Masternodes" />
                         </div>
